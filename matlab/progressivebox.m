89dcc293e2e4ef3e981cf960cbc64e9ff31fc3ae
Martin Loginov <martin.loginov@ut.ee>
2014-06-18 15:56:48 +0300

    A little work on progressive box variant of the algorithm.

--- matlab/progressivebox.m
+++ matlab/progressivebox.m
@@ -1,7 +1,34 @@
-function [ output_args ] = progressivebox( input_args )
+function [ cimage,cratio,mse,psnr ] = progressivebox( img, threshold )
 %PROGRESSIVEWINDOW Summary of this function goes here
 %   Detailed explanation goes here
-
-
+[width, height] = size(img);
+if width ~= height
+    error('Image is not a square');
+end
+cimage = img;
+boxsize=width/2;
+fid=fopen('algorithm.log','w');
+while boxsize > 2
+    for r=1:boxsize:width
+        for c=1:boxsize:height
+            box=cimage(r:r+boxsize-1,c:c+boxsize-1);
+            bmin=min(box(:));
+            bmax=max(box(:));
+            fprintf(fid,'boxsize: %d. min: %d, max: %d\n',boxsize,bmin,bmax);
+            
+            if (bmax-bmin) <= threshold
+                fprintf(fid,'wtf: %d\n',boxsize);
+                box(:,:)=bmin+(bmax-bmin)/2;
+                %comp_ratio=comp_ratio+boxsize*boxsize;
+            end
+            cimage(r:r+boxsize-1,c:c+boxsize-1)=box(:,:);
+        end
+    end
+    boxsize = boxsize/2;
+end
+fclose(fid);
+cratio=0;
+mse=0;
+psnr=0;
 end
 
--- matlab/test.m
+++ matlab/test.m
@@ -4,3 +4,8 @@ lena_orig=imread('../lena512.bmp');
 [asi,ratio,mse,psnr]=boxthreshold(lena_orig,64,16);
 imshow(uint8(asi));
 
+%%
+img=progressivebox(lena_orig,16);
+
+imshow(img);
+
