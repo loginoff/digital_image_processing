59aa8e67eb69136726ddd6ede40a291ab7a4f07f
Martin Loginov <martin.loginov@ut.ee>
2014-06-24 23:35:39 +0300

    First iteration of modified cluster algorithm working

--- matlab/cluster_patch.m
+++ matlab/cluster_patch.m
@@ -1,4 +1,4 @@
-function [out_img] = cluster_img( img, threshold )
+function [out_img,nrclusters] = cluster_img( img, threshold )
 width=size(img,1);
 height=size(img,2);
 visited=zeros(width,height);
@@ -8,58 +8,59 @@ for r=1:width
         if visited(r,c)==0
             clusters=clusters+1;
             current_cluster=[];
-            stack=[];
-            stackptr=0;
+            color=img(r,c);
+            stackx=[r];
+            stacky=[c];
+            stackptr=1;
             while stackptr > 0
-                idx=stack(stackptr);
+                x=stackx(stackptr);
+                y=stacky(stackptr);
                 stackptr=stackptr-1;
                 
-                if idx<1 || idx > width*height
-                    break;
-                end
-                if visited(idx)
-                    break;
-                end
+                 if x<1 || x>width || y < 1 || y > height
+                    continue;
+                 end
+                 if visited(x,y)
+                    continue;
+                 end
+        
+                 idx=(x-1)*width+y;
+                 if size(current_cluster,1)==0
+                    comp=img(idx);
+                 else
+                    comp=img(current_cluster(end));
+                 end
+        
+                 if abs(double(color) - double(img(x,y))) < threshold
+                    %current_cluster(end+1)=idx;
+                    visited(x,y)=clusters;
+                    
+                    for i=-1:1
+                        for j=-1:1
+                            newx=x+i;
+                            newy=y+j;
+                            if newx==x && newy==y
+                                continue
+                            end
+                            if newx<1 || newx>width || newy < 1 || newy > height
+                                continue;
+                            end
+                            if visited(newx,newy)
+                                continue;
+                            end
+                            stackptr=stackptr+1;
+                            stackx(stackptr)=newx;
+                            stacky(stackptr)=newy;
+                        end
+                    end
+                 end
                 
                 
             end
         end
     end
 end
-    %xy to idx
-    %(x-1)*width+y
-    
-    %idx to xy
-    %x=ceil(idx/width);
-    %y=idx-floor(idx/width);
-    function walk(x,y)
-        if x<1 || x>width || y < 1 || y > height
-            return;
-        end
-        if visited(x,y)
-            return;
-        end
-        
-        idx=(x-1)*width+y;
-        if size(current_cluster,1)==0
-            comp=img(idx);
-        else
-            comp=img(current_cluster(end));
-        end
-        
-        if abs(comp - img(idx)) < threshold
-            current_cluster(end+1)=idx;
-            visited(x,y)=clusters;
-            walk(x-1,y-1);
-            walk(x-1,y);
-            walk(x-1,y+1);
-            walk(x,y-1);
-            walk(x,y+1);
-            walk(x+1,y-1);
-            walk(x+1,y);
-            walk(x+1,y+1);
-        end
-    end
 out_img=visited;
+nrclusters=clusters;
 end
 
--- matlab/cluster_wdr.m
+++ matlab/cluster_wdr.m
@@ -1,5 +1,5 @@
-function [ cimage,cratio,mse,psnr ] = cluster_wdr( img, nclusters)
-cimage=cluster_img(img,nclusters);
+function [ cimage,cratio,mse,psnr ] = cluster_wdr( img, threshold)
+cimage=cluster_patch(img,threshold);
 [cratio,bpp]=wcompress('c',cimage,'/dev/shm/tmp2.wdr','wdr');
 cimage=uint8(wcompress('u','/dev/shm/tmp2.wdr'));
 delete('/dev/shm/tmp2.wdr');
